41bd99bcd72c07d353d26946572f839cd3ecd38e
Rahul <rahulmishra4317@gmail.com>
2023-08-03 11:30:00 +0530

    array

---
+++ array/vector.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // vector<int> arr;
+
+    // int ans = (sizeof(arr)/sizeof(int));
+    // cout << ans << endl;
+
+    // cout << arr.size() << endl;
+    // cout << arr.capacity() << endl;
+
+    // //insert 
+    // arr.push_back(1);
+    // arr.push_back(2);
+    // arr.push_back(3);
+    // arr.push_back(4);
+    // arr.push_back(5);
+
+    // for(int i = 0;i<arr.size() ; i++ ){
+    //     cout << arr[i] << endl;
+    // }
+
+    // vector<int> brr(10);
+    // cout << "size is "<< brr.size() << endl;
+    // cout << "capacity is "<<brr.capacity() << endl;
+    
+   
+
+    return 0;
+}
