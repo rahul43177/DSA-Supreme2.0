55f83d26d3c5bdf06b064d6b9d65e0e422c345ab
Rahul Mishra <rahulmishra4317@gmail.com>
2023-12-23 17:21:27 +0000

    solved using 3 pointer method

--- ArrayAssignmentQuestions/sortColor.cpp
+++ ArrayAssignmentQuestions/sortColor.cpp
@@ -1,37 +1,76 @@
-//sort 0 and 1 and 2 ---> 
+// //sort 0 and 1 and 2 ---> 
 
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// void sortColors(vector<int> &arr) {
+//     int size = arr.size();
+//     int zeroCount = 0;
+//     int oneCount = 0;
+//     int twoCount = 0;
+//     for(int i = 0;i<size;i++) {
+//         if(arr[i] == 0) {
+//             zeroCount++ ;
+//         } else if(arr[i] == 1) {
+//             oneCount++ ;
+//         } else {
+//             twoCount++;
+//         }
+//     }
+//     int i = 0;
+//     while(zeroCount--) {
+//         arr[i] = 0;
+//         i++;
+//     } while(oneCount--) {
+//         arr[i] = 1;
+//         i++;
+//     } while(twoCount--) {
+//         arr[i] = 2;
+//         i++;
+//     }
+// }
+
+// void printArray(vector<int> &arr) {
+//     int n = arr.size();
+//     for(int i = 0;i<n;i++) {
+//         cout << arr[i] << " ";
+//     }
+// }
+
+// int main() {
+//     vector<int> arr = {0,1,2,0,0,2,1,2};
+//     sortColors(arr);
+//     printArray(arr);
+// }
+
+
+//doing using 3 pointers
 #include<iostream>
 #include<vector>
 using namespace std;
 
-void sortColors(vector<int> &arr) {
+void sortColor(vector<int> &arr) {
     int size = arr.size();
-    int zeroCount = 0;
-    int oneCount = 0;
-    int twoCount = 0;
-    for(int i = 0;i<size;i++) {
+    int left = 0;
+    int right = size - 1;
+    int i = 0; //index
+
+    while(i <= right) {
         if(arr[i] == 0) {
-            zeroCount++ ;
-        } else if(arr[i] == 1) {
-            oneCount++ ;
+            swap(arr[i] , arr[left]);
+            left ++ ;
+            i++;
+        } else if(arr[i] == 2) {
+            swap(arr[i], arr[right]);
+            right--;
         } else {
-            twoCount++;
+            i++;
         }
-    }
-    int i = 0;
-    while(zeroCount--) {
-        arr[i] = 0;
-        i++;
-    } while(oneCount--) {
-        arr[i] = 1;
-        i++;
-    } while(twoCount--) {
-        arr[i] = 2;
-        i++;
     }
 }
 
-void printArray(vector<int> &arr) {
+void print(vector<int> &arr) {
     int n = arr.size();
     for(int i = 0;i<n;i++) {
         cout << arr[i] << " ";
@@ -39,7 +78,7 @@ void printArray(vector<int> &arr) {
 }
 
 int main() {
-    vector<int> arr = {0,1,2,0,0,2,1,2};
-    sortColors(arr);
-    printArray(arr);
+    vector<int> arr = {0, 1 , 0, 2 , 0, 2 , 1 , 0 , 1 ,2 ,2,1 ,0, 0, 0};
+    sortColor(arr);
+    print(arr);
 }
