471ae24b68acd374a8e73e4a15d55bda1d4b8801
Rahul Mishra <rahulmishra4317@gmail.com>
2023-12-24 20:46:13 +0000

    timepass

---
+++ ArrayAssignmentQuestions/pivotIndex.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int pivotIndex(vector<int> &nums) {
+    
+}
+
+int main() {
+    vector<int> nums= {1,7,3,6,5,6};
+
+
+    pivotIndex(nums);
+}
