9f256257159df001b776c7df56e6501f84a4ae38
Rahul <rahulmishra4317@gmail.com>
2024-02-24 00:34:11 +0530

    factorial using recursion

--- gfg-Mathematics/factorial.js
+++ gfg-Mathematics/factorial.js
@@ -1,9 +1,11 @@
-const findFactorial  = (number) => {
-    if(number == 0) {
-        return 1;
-    }
+const findFact = (number) => {
+  if(number ==0) return 1;
 
-    return (number * findFactorial(number -1));
+  return number * findFact(number-1);
 }
-let number = 4
-console.log(`The factorial of ${number} is ${findFactorial(number)}`)
+
+
+let number = 4;
+let factorial = findFact(number);
+console.log(factorial);
+
