545997584682bd52e9ecfc63d2656ab6dd71dd47
Rahul <rahulmishra4317@gmail.com>
2024-02-29 11:11:02 +0530

    prime factor revise

--- gfg-Mathematics/prime-factor.cpp
+++ gfg-Mathematics/prime-factor.cpp
@@ -1,20 +1,35 @@
 #include<iostream>
 using namespace std;
 
-void findPrimeFactors(int n) {
+void primeFactors(int n) {
     if(n <= 1) return;
-
-    for(int i=2;i*i<=n;i++) {
-        while(n%i == 0) {
-            cout << i << endl;
-            n/=i;
+    
+    while(n%2 ==0) {
+        cout << 2 ;
+        n = n/2;
+    }
+    while(n%3 == 0) {
+        cout << 3;
+        n = n/3;
+    }
+    for(int i = 5;i*i<=n;i+=6) {
+        while(n%i==0) {
+            cout << i ;
+            n = n/i;
+        }
+        while(n%(i+2)==0) {
+            cout << i+2 << " ";
+            n = n/(i+2);
         }
     }
-    if(n>1) {
-        cout << n << endl;
+    if(n>3) {
+        cout << n;
     }
 }
+
+
 int main() {
-    int number = 72;
-    findPrimeFactors(number);
+    int number = 12;
+    primeFactors(number);
+    return 0;
 }
--- gfg-Mathematics/prime-factor.js
+++ gfg-Mathematics/prime-factor.js
@@ -1,77 +1,30 @@
-// function findPrimeFactor(number) {
-//     if(number <=1) return;
-//     for(let i = 2;i*i<=number;i++) {
-//         while(number%i==0) {
-//             console.log(i)
-//             number = Math.floor(number/i);
-//         }
-//     }
-//     if(number > 1) {
-//         console.log(number)
-//     }
-// }
-
-// let number = 12
-
-// findPrimeFactor(number)
-
-//more efficient solution
-// function primeFactor(number) {
-//     if(number <=1) return;
-
-//     while(number%2==0) {
-//         console.log(2)
-//         number = number/2;
-//     }
-//     while(number%3 == 0) {
-//         console.log(3)
-//         number = Math.floor(number/3);
-//     }
-//     for(let i =5;i*i<=number;i+=6){
-//         while(number%i == 0) {
-//             console.log(i)
-//             number = Math.floor(number/i)
-//         }
-//     }
-//     if(number > 3) {
-//         console.log(number)
-//     }
-// }
-
-// const primeFactor = (number) => {
-//     if(number == 1) return;
-//     while(number%2 == 0) {
-//         console.log(2)
-//         number = Math.floor(number/2);
-//     }
-//     while(number%3 == 0) {
-//         console.log(3)
-//         number = Math.floor(number/3)
-//     }
-
-//     for(let i = 5;i*i<= number;i+=6) {
-//         while(number%i == 0) {
-//             console.log(i)
-//             number = Math.floor(number/i)
-//         }
-
-//     }
-//     if(number > 3) {
-//         console.log(number)
-//     }
-// }
-
-// let number  = 25 // ==> 5 5
-// primeFactor(number)
-//count numbers
-
-const countNumber = (n) => {
-    let count = 0;
-    while(n > 0) {
-        count ++;
-        n = Math.floor(n/10);
+const findPrimeFactors = (number) => {
+    if(number <= 1) return;
+    let primeFactorArray = []
+    while(number % 2 == 0) {
+        primeFactorArray.push(2)
+        number = Math.floor(number/2)
     }
-    return count;
-};
+    while(number % 3 == 0) {
+        primeFactorArray.push(3)
+        number = Math.floor(number/3)
+    }
+    for(let i = 5;i*i<=number;i+=6) {
+        while(number % i == 0) {
+            primeFactorArray.push(i)
+            number = math.floor(number / i)
+        }
+        while(number % (i+2) == 0) {
+            primeFactorArray.push(i+2)
+            number = Math.floor(number / (i+2))
+        }
+    }
+    if(number > 3) {
+        primeFactorArray.push(number)
+    }
+    return primeFactorArray.join(",")
+}
 
-console.log(countNumber(123456))
+
+let number = 12 
+console.log(findPrimeFactors(number)) 
